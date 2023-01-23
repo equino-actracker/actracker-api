ada3db0b92868a7c6621e9878c6e1fc4d91baf08
marcinciapa <aaryndo@gmail.com>
2023-01-23 23:08:05 +0100

    Entity validation (#20)
    * Add Activity validation
    
    * Version - patch

---
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/activity/ActivityId.java
@@ -0,0 +1,19 @@
+package ovh.equino.actracker.domain.activity;
+
+import java.util.UUID;
+
+import static java.util.Objects.isNull;
+import static java.util.UUID.randomUUID;
+
+record ActivityId(UUID id) {
+
+    ActivityId {
+        if (isNull(id)) {
+            throw new IllegalArgumentException("ActivityId.id must is null");
+        }
+    }
+
+    ActivityId() {
+        this(randomUUID());
+    }
+}
---
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/activity/ActivityValidator.java
@@ -0,0 +1,37 @@
+package ovh.equino.actracker.domain.activity;
+
+import ovh.equino.actracker.domain.activity.error.ActivityInvalidException;
+
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.List;
+
+class ActivityValidator {
+
+    private final Activity activity;
+    private final List<String> validationErrors = new LinkedList<>();
+
+    ActivityValidator(Activity activity) {
+        this.activity = activity;
+    }
+
+    void validate() {
+        if (endTimeBeforeStartTime()) {
+            validationErrors.add("End time is before start time");
+        }
+
+        if (!validationErrors.isEmpty()) {
+            throw new ActivityInvalidException(validationErrors);
+        }
+    }
+
+    private boolean endTimeBeforeStartTime() {
+        Instant activityStartTime = activity.getStartTime();
+        Instant activityEndTime = activity.getEndTime();
+
+        if (activityStartTime == null || activityEndTime == null) {
+            return false;
+        }
+        return activityEndTime.isBefore(activityStartTime);
+    }
+}
