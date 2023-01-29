cb0f730ca75395821141a614ffc417fad221406b
Marcin Ciapa <aaryndo@gmail.com>
2023-01-29 18:21:51 +0100

    Add second local user

--- actracker-api-db-h2/src/main/resources/h2Data.sql
+++ actracker-api-db-h2/src/main/resources/h2Data.sql
@@ -1,9 +1,19 @@
 INSERT INTO tenant(
-        id,
-        username,
-        password
-    ) VALUES (
-        'f94cdf1c-f9e4-45df-b92e-5390b40f9ea1',
-        'user1',
-        '$2y$10$QuJzfMuUHV4gSdrGbW3R9u63nGlsp8CI1JBzzgXW9tJKGx0cniViW'
-    );
+    id,
+    username,
+    password
+) VALUES (
+    'f94cdf1c-f9e4-45df-b92e-5390b40f9ea1',
+    'user1',
+    '$2y$10$QuJzfMuUHV4gSdrGbW3R9u63nGlsp8CI1JBzzgXW9tJKGx0cniViW'
+);
+
+INSERT INTO tenant(
+    id,
+    username,
+    password
+) VALUES (
+    'b2b48246-44f1-4e50-82ec-da0a116d889b',
+    'user2',
+    '$2y$10$Fv5oOn/AfiPhwAavq9w5WOZ/jtVG580gIfruyRS2xG2KkUx8CfiVq'
+);
