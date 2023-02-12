a53c35cf2433fa49c6ee0505f74873b264a4661e
marcinciapa <aaryndo@gmail.com>
2023-02-12 10:25:12 +0100

    Remove service interface from domain module (#24)

--- actracker-api-domain/src/main/java/ovh/equino/actracker/domain/activity/ActivityService.java
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/activity/ActivityService.java
@@ -1,15 +1,41 @@
 package ovh.equino.actracker.domain.activity;
 
+import ovh.equino.actracker.domain.activity.error.ActivityNotFoundException;
 import ovh.equino.actracker.domain.user.User;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ActivityService {
+public class ActivityService {
 
-    ActivityDto createActivity(ActivityDto newActivityData, User creator);
+    private final ActivityRepository activityRepository;
 
-    ActivityDto updateActivity(UUID activityId, ActivityDto updatedActivityData, User updater);
+    ActivityService(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
 
-    List<ActivityDto> getActivities(User searcher);
+    public ActivityDto createActivity(ActivityDto newActivityData, User creator) {
+        Activity createdActivity = new Activity(new ActivityId(), newActivityData, creator);
+        activityRepository.add(createdActivity.toDto());
+        return createdActivity.toDto();
+    }
+
+    public ActivityDto updateActivity(UUID activityId, ActivityDto updatedActivityData, User updater) {
+        ActivityDto existingActivityData = activityRepository.findById(activityId)
+                .orElseThrow(ActivityNotFoundException::new);
+
+        Activity activity = new Activity(existingActivityData);
+
+        if (activity.isNotAvailableFor(updater)) {
+            throw new ActivityNotFoundException();
+        }
+
+        activity.updateTo(updatedActivityData);
+        activityRepository.udpate(activityId, activity.toDto());
+        return activity.toDto();
+    }
+
+    public List<ActivityDto> getActivities(User searcher) {
+        return activityRepository.findAll(searcher);
+    }
 }
