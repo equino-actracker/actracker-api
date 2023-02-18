af00d87f3452894c5487364dfd943a56e06e2372
Marcin Ciapa <aaryndo@gmail.com>
2023-02-18 18:25:28 +0100

    Setup set version trigger for outbox notification in Postgres

--- actracker-api-db-postgres/src/main/resources/schema/V3__outbox_notification.sql
+++ actracker-api-db-postgres/src/main/resources/schema/V3__outbox_notification.sql
@@ -5,3 +5,18 @@ CREATE TABLE IF NOT EXISTS outbox_notification (
     version     INTEGER         NOT NULL        ,
     entity      TEXT
 );
+
+CREATE OR REPLACE FUNCTION outbox_notification_created_handler()
+    RETURNS TRIGGER
+    AS
+$$
+BEGIN
+    NEW.version := nextval('outbox_notification_version_seq');
+    RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER outbox_notification_created_trg
+    BEFORE INSERT OR UPDATE
+    ON outbox_notification
+    FOR EACH ROW EXECUTE FUNCTION outbox_notification_created_handler();
