1155a7b540aef4b1efcb0b0ec0d1706268de18d4
Marcin Ciapa <aaryndo@gmail.com>
2023-02-25 15:27:25 +0100

    Use ActivityDto as notification entity

--- actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/outbox/NotificationMapper.java
+++ actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/outbox/NotificationMapper.java
@@ -3,6 +3,7 @@ package ovh.equino.actracker.repository.jpa.outbox;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import ovh.equino.actracker.domain.activity.ActivityDto;
 import ovh.equino.actracker.notification.outbox.Notification;
 
 import java.util.UUID;
@@ -18,8 +19,8 @@ class NotificationMapper {
 
     Notification toDto(NotificationEntity entity) {
         try {
-            Notification notification = objectMapper.readValue(entity.entity, Notification.class);
-            return new Notification(UUID.fromString(entity.id), notification);
+            ActivityDto activity = objectMapper.readValue(entity.entity, ActivityDto.class);
+            return new Notification(UUID.fromString(entity.id), activity);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
@@ -29,7 +30,7 @@ class NotificationMapper {
         try {
             NotificationEntity entity = new NotificationEntity();
             entity.id = dto.id().toString();
-            entity.entity = objectMapper.writeValueAsString(dto);
+            entity.entity = objectMapper.writeValueAsString(dto.entity());
             return entity;
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
