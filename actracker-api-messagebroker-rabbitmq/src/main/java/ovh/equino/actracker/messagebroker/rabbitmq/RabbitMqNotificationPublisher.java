c83353249ac967c521675ed261c48d058178a20d
Marcin Ciapa <aaryndo@gmail.com>
2023-03-02 17:16:53 +0100

    Publish to exchange instead of queue

--- actracker-api-messagebroker-rabbitmq/src/main/java/ovh/equino/actracker/messagebroker/rabbitmq/RabbitMqNotificationPublisher.java
+++ actracker-api-messagebroker-rabbitmq/src/main/java/ovh/equino/actracker/messagebroker/rabbitmq/RabbitMqNotificationPublisher.java
@@ -3,13 +3,17 @@ package ovh.equino.actracker.messagebroker.rabbitmq;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 import ovh.equino.actracker.domain.activity.ActivityChangedNotification;
 import ovh.equino.actracker.notification.outbox.NotificationPublisher;
 
 import java.io.IOException;
 
+import static com.rabbitmq.client.BuiltinExchangeType.FANOUT;
+
 class RabbitMqNotificationPublisher implements NotificationPublisher {
 
+    public static final String EXCHANGE_NAME = "notification.X";
     private final Channel channel;
     private final ObjectMapper objectMapper;
 
@@ -18,7 +22,7 @@ class RabbitMqNotificationPublisher implements NotificationPublisher {
         this.objectMapper.registerModule(new JavaTimeModule());
         this.channel = channelFactory.createChannel();
         try {
-            this.channel.queueDeclare("notification.Q", false, false, false, null);
+            this.channel.exchangeDeclare(EXCHANGE_NAME, FANOUT);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
@@ -28,7 +32,7 @@ class RabbitMqNotificationPublisher implements NotificationPublisher {
     public void publishNotification(ActivityChangedNotification changedNotification) {
         try {
             String message = objectMapper.writeValueAsString(changedNotification);
-            channel.basicPublish("", "notification.Q", null, message.getBytes());
+            channel.basicPublish(EXCHANGE_NAME, "", MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
