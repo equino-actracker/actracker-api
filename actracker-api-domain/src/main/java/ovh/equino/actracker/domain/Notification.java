bdda7095915d30c560de10e4837869c47a34cf90
Marcin Ciapa <aaryndo@gmail.com>
2023-03-31 16:17:34 +0200

    Deliver notification serializer/deserializer (#34)
    * Deliver notification serializer/deserializer
    
    * Stop failing notification parsing on unknown properties
    
    * Refactor notification deserializing

--- actracker-api-domain/src/main/java/ovh/equino/actracker/domain/Notification.java
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/Notification.java
@@ -1,10 +1,17 @@
 package ovh.equino.actracker.domain;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import ovh.equino.actracker.domain.exception.ParseException;
+
 import java.util.UUID;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static java.util.Objects.requireNonNull;
 
-public record Notification<T> (
+public record Notification<T>(
         UUID id,
         long version,
         T data,
@@ -22,6 +29,38 @@ public record Notification<T> (
     public Notification {
         requireNonNull(id);
         requireNonNull(data);
+        requireNonNull(notificationType);
     }
 
+    public String toJson() throws ParseException {
+        try {
+            return objectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new ParseException(e);
+        }
+    }
+
+    public static Notification<?> fromJson(String json) throws ParseException {
+        try {
+            JavaType notificationType = getNotificationType(json);
+            return objectMapper().readValue(json, notificationType);
+        } catch (JsonProcessingException e) {
+            throw new ParseException(e);
+        }
+    }
+
+    private static JavaType getNotificationType(String rawMessage) throws JsonProcessingException {
+        Notification<?> notification = objectMapper().readValue(rawMessage, Notification.class);
+        return objectMapper().getTypeFactory().constructParametricType(
+                Notification.class,
+                notification.notificationType()
+        );
+    }
+
+    private static ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
 }
---
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/exception/ParseException.java
@@ -0,0 +1,8 @@
+package ovh.equino.actracker.domain.exception;
+
+public class ParseException extends Exception {
+
+    public ParseException(Throwable cause) {
+        super(cause);
+    }
+}
