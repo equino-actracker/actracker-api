a0e1fa955e73b5207fa4be02054f9a807f068df0
Marcin Ciapa <aaryndo@gmail.com>
2023-04-19 19:44:15 +0200

    Implement tag service logic

--- actracker-api-domain/src/main/java/ovh/equino/actracker/domain/tagset/TagSetServiceImpl.java
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/tagset/TagSetServiceImpl.java
@@ -2,9 +2,12 @@ package ovh.equino.actracker.domain.tagset;
 
 import ovh.equino.actracker.domain.EntitySearchCriteria;
 import ovh.equino.actracker.domain.EntitySearchResult;
+import ovh.equino.actracker.domain.exception.EntityNotFoundException;
 import ovh.equino.actracker.domain.tag.TagRepository;
+import ovh.equino.actracker.domain.tag.TagsExistenceVerifier;
 import ovh.equino.actracker.domain.user.User;
 
+import java.util.List;
 import java.util.UUID;
 
 class TagSetServiceImpl implements TagSetService {
@@ -24,21 +27,51 @@ class TagSetServiceImpl implements TagSetService {
 
     @Override
     public TagSetDto createTagSet(TagSetDto newTagSetData, User creator) {
-        throw new IllegalStateException("Not implemented yet");
+        TagsExistenceVerifier tagsExistenceVerifier = new TagsExistenceVerifier(tagRepository, creator);
+        TagSet tagSet = TagSet.create(newTagSetData, creator, tagsExistenceVerifier);
+        tagSetRepository.add(tagSet.forStorage());
+        return tagSet.forClient();
     }
 
     @Override
     public TagSetDto updateTagSet(UUID tagSetId, TagSetDto updatedTagSetData, User updater) {
-        throw new IllegalStateException("Not implemented yet");
+        TagSet tagSet = getTagSetIfAuthorized(updater, tagSetId);
+        tagSet.updateTo(updatedTagSetData);
+        tagSetRepository.update(tagSetId, tagSet.forStorage());
+        return tagSet.forClient();
     }
 
     @Override
     public EntitySearchResult<TagSetDto> searchTagSets(EntitySearchCriteria searchCriteria) {
-        throw new IllegalStateException("Not implemented yet");
+        TagsExistenceVerifier tagsExistenceVerifier = new TagsExistenceVerifier(tagRepository, searchCriteria.searcher());
+
+        EntitySearchResult<TagSetDto> searchResult = tagSetSearchEngine.findTagSets(searchCriteria);
+        List<TagSetDto> resultForClient = searchResult.results().stream()
+                .map(tagSet -> TagSet.fromStorage(tagSet, tagsExistenceVerifier))
+                .map(TagSet::forClient)
+                .toList();
+
+        return new EntitySearchResult<>(searchResult.nextPageId(), resultForClient);
     }
 
     @Override
     public void deleteTagSet(UUID tagSetId, User remover) {
-        throw new IllegalStateException("Not implemented yet");
+        TagSet tagSet = getTagSetIfAuthorized(remover, tagSetId);
+        tagSet.delete();
+        tagSetRepository.update(tagSetId, tagSet.forStorage());
+    }
+
+    private TagSet getTagSetIfAuthorized(User user, UUID tagSetId) {
+        TagsExistenceVerifier tagsExistenceVerifier = new TagsExistenceVerifier(tagRepository, user);
+
+        TagSetDto tagSetDto = tagSetRepository.findById(tagSetId)
+                .orElseThrow(() -> new EntityNotFoundException(TagSet.class, tagSetId));
+
+        TagSet tagSet = TagSet.fromStorage(tagSetDto, tagsExistenceVerifier);
+
+        if (tagSet.isNotAvailableFor(user)) {
+            throw new EntityNotFoundException(TagSet.class, tagSetId);
+        }
+        return tagSet;
     }
 }
