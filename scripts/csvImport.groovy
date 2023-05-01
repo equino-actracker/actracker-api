51dbe2787daf63f05bbb8297730ac090f82475b0
Marcin Ciapa <aaryndo@gmail.com>
2023-05-01 21:10:50 +0200

    Hide tag names/IDs

--- scripts/csvImport.groovy
+++ scripts/csvImport.groovy
@@ -5,12 +5,9 @@ import java.time.format.DateTimeFormatter
 String basicAuthToken = ''
 
 def tagToId = [
-        'M': '9b3f3139-beef-4cf0-aa28-a4264c2804de',
-        'S': '6816f216-eb07-4c9a-958c-46fec4c49aad',
-        'A': 'cce01f73-67a5-46dc-982e-3e7ab1da1865',
-        'R': 'afee734e-ecb5-4758-8fd4-e389a7b23111',
-        'P': '93b2378e-a3d0-411e-8936-e84d4ac0c6cd',
-        'D': 'd10dc389-1181-412c-95d6-61e9e7ce5b18'
+        'tagName1': 'tagId1',
+        'tagName2': 'tagId2',
+        'tagName3': 'tagId3',
 ]
 
 def file = new File('actracker.csv')
