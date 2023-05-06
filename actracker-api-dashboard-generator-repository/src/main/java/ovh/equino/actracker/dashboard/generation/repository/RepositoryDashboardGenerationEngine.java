b3eae4bdca0c39813f5886fa527fc10472644a74
Marcin Ciapa <aaryndo@gmail.com>
2023-05-06 19:39:00 +0200

    Generate daily dashboard

--- actracker-api-dashboard-generator-repository/src/main/java/ovh/equino/actracker/dashboard/generation/repository/RepositoryDashboardGenerationEngine.java
+++ actracker-api-dashboard-generator-repository/src/main/java/ovh/equino/actracker/dashboard/generation/repository/RepositoryDashboardGenerationEngine.java
@@ -6,10 +6,17 @@ import ovh.equino.actracker.domain.dashboard.*;
 import ovh.equino.actracker.domain.tag.TagDto;
 import ovh.equino.actracker.domain.tag.TagSearchEngine;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import static java.time.temporal.ChronoUnit.DAYS;
 import static java.util.Collections.emptyList;
+import static java.util.Objects.nonNull;
 import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static ovh.equino.actracker.dashboard.generation.repository.DashboardUtils.*;
 
 class RepositoryDashboardGenerationEngine implements DashboardGenerationEngine {
 
@@ -23,37 +30,135 @@ class RepositoryDashboardGenerationEngine implements DashboardGenerationEngine {
 
     @Override
     public DashboardData generateDashboard(DashboardDto dashboard, DashboardGenerationCriteria generationCriteria) {
+
+        List<TagDto> tags = tagFinder.find(generationCriteria);
+        if (isEmpty(tags)) {
+            return empty(dashboard);
+        }
+
+        List<ActivityDto> activities = activityFinder.find(generationCriteria).stream()
+                .filter(activity -> isNotEmpty(activity.tags()))
+                .toList();
+
+        Instant earliestStartTime = activities.stream()
+                .filter(activity -> isNotEmpty(activity.tags()))
+                .map(ActivityDto::startTime)
+                .filter(Objects::nonNull)
+                .min(Instant::compareTo)
+                .orElse(null);
+        if (earliestStartTime == null) {
+            return empty(dashboard);
+        }
+        earliestStartTime = latestOf(beginningOfDay(earliestStartTime), generationCriteria.timeRangeStart());
+
+        Instant latestEndTime = activities.stream()
+                .filter(activity -> isNotEmpty(activity.tags()))
+                .map(ActivityDto::endTime)
+                .filter(Objects::nonNull)
+                .max(Instant::compareTo)
+                .orElse(null);
+        latestEndTime = earliestOf(beginningOfDay(latestEndTime), generationCriteria.timeRangeEnd());
+        if (latestEndTime == null) {
+            return empty(dashboard);
+        }
+
+        DashboardGenerationCriteria dataDrivenGenerationCriteria = new DashboardGenerationCriteria(
+                generationCriteria.dashboardId(),
+                generationCriteria.generator(),
+                earliestStartTime,
+                latestEndTime
+        );
+
         List<DashboardChartData> chartsData = dashboard.charts().stream()
-                .map(chart -> generate(chart, generationCriteria))
+                .map(chart -> generate(chart, dataDrivenGenerationCriteria, tags, activities))
                 .toList();
 
         return new DashboardData(dashboard.name(), chartsData);
     }
 
-    private DashboardChartData generate(Chart chart, DashboardGenerationCriteria generationCriteria) {
-        return generateChartGroupedByTag(chart.name(), generationCriteria);
-//        return switch (chart.groupBy()) {
-//            case TAG -> dashboardRepository.generateChartGroupedByTags(chart.name(), generationCriteria);
-//            case DAY -> dashboardRepository.generateChartGroupedByDays(chart.name(), generationCriteria);
-//        };
+    private DashboardChartData generate(Chart chart,
+                                        DashboardGenerationCriteria generationCriteria,
+                                        List<TagDto> allTags,
+                                        List<ActivityDto> allActivities) {
+
+        return switch (chart.groupBy()) {
+            case TAG -> generateChartGroupedByTag(chart.name(), generationCriteria, allTags, allActivities);
+            case DAY -> generateChartGroupedByDay(chart.name(), generationCriteria, allTags, allActivities);
+        };
     }
 
     private DashboardChartData generateChartGroupedByTag(String chartName,
-                                                         DashboardGenerationCriteria generationCriteria) {
+                                                         DashboardGenerationCriteria generationCriteria,
+                                                         List<TagDto> allTags,
+                                                         List<ActivityDto> allActivities) {
 
-        List<TagDto> tags = tagFinder.find(generationCriteria);
-        if (isEmpty(tags)) {
-            new DashboardChartData(chartName, emptyList());
-        }
+        List<ActivityDto> matchingAlignedActivities = alignedTo(generationCriteria, allActivities);
 
-        List<ActivityDto> activities = activityFinder.find(generationCriteria);
-        if (isEmpty(activities)) {
-            new DashboardChartData(chartName, emptyList());
-        }
-
-        List<ChartBucketData> buckets = new TagBucketsGenerator().generate(tags, activities);
+        List<ChartBucketData> buckets = new TagBucketsGenerator().generate(allTags, matchingAlignedActivities);
 
         return new DashboardChartData(chartName, buckets);
     }
 
+    private DashboardChartData generateChartGroupedByDay(String chartName,
+                                                         DashboardGenerationCriteria generationCriteria,
+                                                         List<TagDto> allTags,
+                                                         List<ActivityDto> allActivities) {
+
+        List<ChartBucketData> dailyBuckets = new ArrayList<>();
+
+        for (Instant bucket = generationCriteria.timeRangeStart();
+             bucket.isBefore(generationCriteria.timeRangeEnd());
+             bucket = bucket.plus(1, DAYS)) {
+
+            DashboardGenerationCriteria forDayGenerationCriteria = new DashboardGenerationCriteria(
+                    generationCriteria.dashboardId(),
+                    generationCriteria.generator(),
+                    bucket,
+                    bucket.plus(1, DAYS).minusMillis(1)
+            );
+
+            List<ActivityDto> matchingAlignedActivities = alignedTo(forDayGenerationCriteria, allActivities);
+
+            DashboardChartData chartByTag = generateChartGroupedByTag(
+                    Long.toString(bucket.toEpochMilli()),
+                    forDayGenerationCriteria,
+                    allTags,
+                    matchingAlignedActivities
+            );
+
+            ChartBucketData dailyBucket = new ChartBucketData(
+                    Long.toString(bucket.toEpochMilli()),
+                    ChartBucketData.BucketType.DAY,
+                    null,
+                    null,
+                    chartByTag.buckets()
+            );
+            dailyBuckets.add(dailyBucket);
+        }
+
+        return new DashboardChartData(chartName, dailyBuckets);
+    }
+
+    DashboardData empty(DashboardDto dashboard) {
+        List<DashboardChartData> emptyCharts = dashboard.charts()
+                .stream()
+                .map(chart -> new DashboardChartData(chart.name(), emptyList()))
+                .toList();
+        return new DashboardData(dashboard.name(), emptyCharts);
+    }
+
+    private List<ActivityDto> alignedTo(DashboardGenerationCriteria generationCriteria, List<ActivityDto> activities) {
+        return activities.stream()
+                .filter(activity -> nonNull(activity.startTime()))
+//                .filter(activity -> activity.startTime().isBefore(generationCriteria.timeRangeEnd()))
+//                .filter(activity -> activity.endTime().isAfter(generationCriteria.timeRangeStart()))
+                .map(activity -> new ActivityDto(
+                        activity.title(),
+                        latestOf(activity.startTime(), generationCriteria.timeRangeStart()),
+                        earliestOf(activity.endTime(), generationCriteria.timeRangeEnd()),
+                        activity.comment(),
+                        activity.tags()
+                ))
+                .toList();
+    }
 }
