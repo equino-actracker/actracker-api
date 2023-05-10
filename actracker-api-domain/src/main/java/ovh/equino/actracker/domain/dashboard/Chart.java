e98f12ee74762dc7b5d5726307940b9bf88c3709
Marcin Ciapa <aaryndo@gmail.com>
2023-05-10 23:14:20 +0200

    Consider included tags during report generation

--- actracker-api-domain/src/main/java/ovh/equino/actracker/domain/dashboard/Chart.java
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/dashboard/Chart.java
@@ -3,12 +3,23 @@ package ovh.equino.actracker.domain.dashboard;
 import java.util.Set;
 import java.util.UUID;
 
+import static java.util.Collections.emptySet;
+import static java.util.Objects.requireNonNullElse;
+
 public record Chart(
 
         String name,
         GroupBy groupBy,
         Set<UUID> includedTags
 ) {
+
+    public Chart {
+        includedTags = requireNonNullElse(includedTags, emptySet());
+    }
+
+    public boolean includesAllTags() {
+        return includedTags.isEmpty();
+    }
 
     public enum GroupBy {
         TAG,
