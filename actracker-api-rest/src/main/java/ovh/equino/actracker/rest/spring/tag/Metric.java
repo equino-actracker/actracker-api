1fc7a7d947e7431d9d99de18a45e684f7dead05f
Marcin Ciapa <aaryndo@gmail.com>
2023-07-26 21:32:44 +0200

    Remove domain dependency from rest module

--- actracker-api-rest/src/main/java/ovh/equino/actracker/rest/spring/tag/Metric.java
+++ actracker-api-rest/src/main/java/ovh/equino/actracker/rest/spring/tag/Metric.java
@@ -1,24 +1,6 @@
 package ovh.equino.actracker.rest.spring.tag;
 
-record Metric(
-        String id,
-        String name,
-        String type
-) {
-
-    enum MetricType {
-        NUMERIC;
-
-        static MetricType fromDomain(ovh.equino.actracker.domain.tag.MetricType metricType) {
-            return switch (metricType) {
-                case NUMERIC -> NUMERIC;
-            };
-        }
-
-        static ovh.equino.actracker.domain.tag.MetricType toDomain(MetricType metricType) {
-            return switch (metricType) {
-                case NUMERIC -> ovh.equino.actracker.domain.tag.MetricType.NUMERIC;
-            };
-        }
-    }
+record Metric(String id,
+              String name,
+              String type) {
 }
