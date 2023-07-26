c1a73fed5171eb1efb6ca325c835183b7f65fe64
Marcin Ciapa <aaryndo@gmail.com>
2023-07-26 21:30:53 +0200

    Remove rest mappers

--- actracker-api-rest/src/main/java/ovh/equino/actracker/rest/spring/share/ShareMapper.java
+++ actracker-api-rest/src/main/java/ovh/equino/actracker/rest/spring/share/ShareMapper.java
@@ -2,33 +2,6 @@ package ovh.equino.actracker.rest.spring.share;
 
 import ovh.equino.actracker.rest.spring.PayloadMapper;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import static java.util.Objects.requireNonNullElse;
-
 public class ShareMapper extends PayloadMapper {
 
-    public List<ovh.equino.actracker.domain.share.Share> fromRequest(Collection<Share> shares) {
-        return requireNonNullElse(shares, new ArrayList<Share>())
-                .stream()
-                .map(this::fromRequest)
-                .toList();
-    }
-
-    public ovh.equino.actracker.domain.share.Share fromRequest(Share share) {
-        return new ovh.equino.actracker.domain.share.Share(share.granteeName());
-    }
-
-    public List<Share> toResponse(Collection<ovh.equino.actracker.domain.share.Share> shares) {
-        return requireNonNullElse(shares, new ArrayList<ovh.equino.actracker.domain.share.Share>())
-                .stream()
-                .map(this::toResponse)
-                .toList();
-    }
-
-    Share toResponse(ovh.equino.actracker.domain.share.Share share) {
-        return new Share(share.granteeName());
-    }
 }
--- actracker-api-rest/src/main/java/ovh/equino/actracker/rest/spring/tag/MetricMapper.java
+++ actracker-api-rest/src/main/java/ovh/equino/actracker/rest/spring/tag/MetricMapper.java
@@ -1,42 +1,7 @@
 package ovh.equino.actracker.rest.spring.tag;
 
-import ovh.equino.actracker.domain.tag.MetricDto;
-import ovh.equino.actracker.domain.tag.MetricType;
 import ovh.equino.actracker.rest.spring.PayloadMapper;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import static java.util.Objects.requireNonNullElse;
 
 class MetricMapper extends PayloadMapper {
 
-    List<MetricDto> fromRequest(Collection<Metric> metrics) {
-        return requireNonNullElse(metrics, new ArrayList<Metric>()).stream()
-                .map(this::fromRequest)
-                .toList();
-    }
-
-    MetricDto fromRequest(Metric metric) {
-        return new MetricDto(
-                stringToUuid(metric.id()),
-                metric.name(),
-                MetricType.valueOf(metric.type())
-        );
-    }
-
-    List<Metric> toResponse(Collection<MetricDto> metrics) {
-        return requireNonNullElse(metrics, new ArrayList<MetricDto>()).stream()
-                .map(this::toResponse)
-                .toList();
-    }
-
-    Metric toResponse(MetricDto metric) {
-        return new Metric(
-                uuidToString(metric.id()),
-                metric.name(),
-                metric.type().toString()
-        );
-    }
 }
