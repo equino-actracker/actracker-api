437bdeabc3903bd25e8c1e7e0d252be8fb17bb74
Marcin Ciapa <aaryndo@gmail.com>
2023-10-27 22:13:57 +0200

    Implement search method in tag set data source

--- actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/tagset/JpaTagSetDataSource.java
+++ actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/tagset/JpaTagSetDataSource.java
@@ -1,6 +1,7 @@
 package ovh.equino.actracker.repository.jpa.tagset;
 
 import jakarta.persistence.Tuple;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import ovh.equino.actracker.domain.EntitySearchCriteria;
 import ovh.equino.actracker.domain.tagset.TagSetDataSource;
@@ -12,8 +13,11 @@ import ovh.equino.actracker.repository.jpa.tag.TagEntity;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static jakarta.persistence.criteria.JoinType.LEFT;
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 class JpaTagSetDataSource extends JpaDAO implements TagSetDataSource {
 
@@ -56,7 +60,59 @@ class JpaTagSetDataSource extends JpaDAO implements TagSetDataSource {
     @Override
     public List<TagSetDto> find(EntitySearchCriteria searchCriteria) {
 
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createTupleQuery();
 
-        return null;
+        Root<TagSetEntity> tagSet = criteriaQuery.from(TagSetEntity.class);
+        Join<TagSetEntity, TagEntity> tag = tagSet.join("tags", LEFT);
+
+        // TODO replace get by string with generated JPA Meta Model
+        Predicate isAccessibleForSearcher = criteriaBuilder.equal(tagSet.get("creatorId"), searchCriteria.searcher().id().toString());
+        Predicate isTagSetNotDeleted = criteriaBuilder.isFalse(tagSet.get("deleted"));
+        Predicate isTagNotDeleted = criteriaBuilder.or(
+                criteriaBuilder.isFalse(tag.get("deleted")),
+                criteriaBuilder.isNull(tag.get("id"))
+        );
+        Predicate isInPage;
+        if (isBlank(searchCriteria.pageId())) {
+            isInPage = criteriaBuilder.and();
+        } else {
+            isInPage = criteriaBuilder.greaterThanOrEqualTo(
+                    tagSet.get("id"),
+                    searchCriteria.pageId()
+            );
+        }
+        Predicate isNotExcluded;
+        if (isEmpty(searchCriteria.excludeFilter())) {
+            isNotExcluded = criteriaBuilder.and();
+        } else {
+            Path<Object> id = tagSet.get("id");
+            CriteriaBuilder.In<Object> idIn = criteriaBuilder.in(id);
+            searchCriteria.excludeFilter().stream()
+                    .map(UUID::toString)
+                    .forEach(idIn::value);
+            isNotExcluded = criteriaBuilder.not(idIn);
+        }
+
+        CriteriaQuery<Tuple> query = criteriaQuery.multiselect(tagSet.alias("tagSetAlias"))
+                .where(
+                        criteriaBuilder.and(
+                                isAccessibleForSearcher,
+                                isTagSetNotDeleted,
+                                isTagNotDeleted,
+                                isInPage,
+                                isNotExcluded
+                        )
+                );
+
+        TypedQuery<Tuple> typedQuery = entityManager
+                .createQuery(query)
+                .setMaxResults(searchCriteria.pageSize());
+
+        return typedQuery
+                .getResultStream()
+                .map(result -> result.get("tagSetAlias", TagSetEntity.class))
+                .map(mapper::toDto)
+                .toList();
     }
 }
