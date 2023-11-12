5579982ae5e691712d78d3c8902a190d53328fcc
Marcin Ciapa <aaryndo@gmail.com>
2023-11-12 17:22:01 +0100

    Remove distinct from select metrics query

--- actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/activity/SelectMetricValuesQuery.java
+++ actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/activity/SelectMetricValuesQuery.java
@@ -41,8 +41,7 @@ final class SelectMetricValuesQuery extends MultiResultJpaQuery<MetricValueEntit
                                 metric.get("id"),
                                 root.get("value")
                         )
-                )
-                .distinct(true);
+                );
     }
 
     @Override
