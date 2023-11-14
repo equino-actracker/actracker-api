535933c2b08e8b80ab6a1cde5260d9356d64ba35
Marcin Ciapa <aaryndo@gmail.com>
2023-11-14 19:42:27 +0100

    Prepare integration test configuration - integration test framework

---
+++ actracker-api-repository-jpa/src/integration-test/java/ovh/equino/actracker/repository/jpa/IntegrationTestTagsConfiguration.java
@@ -0,0 +1,41 @@
+package ovh.equino.actracker.repository.jpa;
+
+import ovh.equino.actracker.domain.tag.TagDto;
+import ovh.equino.actracker.domain.tenant.TenantDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.function.Predicate.not;
+import static java.util.stream.Stream.concat;
+
+public class IntegrationTestTagsConfiguration {
+
+    private final List<TagDto> addedTags = new ArrayList<>();
+    private final List<TagDto> transientTags = new ArrayList<>();
+
+    void persistIn(IntegrationTestRelationalDataBase database) throws SQLException {
+        database.addTags(addedTags.toArray(new TagDto[0]));
+    }
+
+    public void add(TagDto tag) {
+        addedTags.add(tag);
+    }
+
+    public void addTransient(TagDto tag) {
+        transientTags.add(tag);
+    }
+
+    public Collection<TagDto> accessibleFor(TenantDto user) {
+        return addedTags;
+    }
+
+    public Collection<TagDto> inaccessibleFor(TenantDto user) {
+        Collection<TagDto> accessibleTags = accessibleFor(user);
+        return concat(addedTags.stream(), transientTags.stream())
+                .filter(not(accessibleTags::contains))
+                .toList();
+    }
+}
