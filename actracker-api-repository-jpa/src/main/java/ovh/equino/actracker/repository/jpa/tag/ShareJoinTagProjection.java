f4bb6aae2737e9d12ab1d868a07209b17937c3cb
Marcin Ciapa <aaryndo@gmail.com>
2023-11-16 22:31:27 +0100

    Refactor tag JPA data source

--- actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/tag/ShareJoinTagProjection.java
+++ actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/tag/ShareJoinTagProjection.java
@@ -1,4 +1,18 @@
 package ovh.equino.actracker.repository.jpa.tag;
 
-public record ShareJoinTagProjection(String granteeId, String tagId, String granteeName) {
+import ovh.equino.actracker.domain.share.Share;
+import ovh.equino.actracker.domain.user.User;
+
+import java.util.UUID;
+
+import static java.util.Objects.nonNull;
+
+record ShareJoinTagProjection(String granteeId, String tagId, String granteeName) {
+
+    Share toShare() {
+        User granteeId = nonNull(granteeId())
+                ? new User(UUID.fromString(granteeId()))
+                : null;
+        return new Share(granteeId, granteeName());
+    }
 }
