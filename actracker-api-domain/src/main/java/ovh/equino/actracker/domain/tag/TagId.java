b953238efd90fbaaa513eb124f4426710a6895d8
Marcin Ciapa <aaryndo@gmail.com>
2023-11-25 09:32:11 +0100

    Clean up ID value objects

--- actracker-api-domain/src/main/java/ovh/equino/actracker/domain/tag/TagId.java
+++ actracker-api-domain/src/main/java/ovh/equino/actracker/domain/tag/TagId.java
@@ -1,6 +1,5 @@
 package ovh.equino.actracker.domain.tag;
 
-import java.util.Objects;
 import java.util.UUID;
 
 import static java.util.Objects.isNull;
@@ -16,23 +15,5 @@ public record TagId(UUID id) {
 
     public TagId() {
         this(randomUUID());
-    }
-
-    @Override
-    public String toString() {
-        return id.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TagId tagId = (TagId) o;
-        return Objects.equals(id, tagId.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
