e52aebed67ac0cdd5705c7263db4366cad4ae971
Marcin Ciapa <aaryndo@gmail.com>
2023-11-26 14:08:52 +0100

    Map notification projection to DTO

--- actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/notification/NotificationProjection.java
+++ actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/notification/NotificationProjection.java
@@ -1,4 +1,15 @@
 package ovh.equino.actracker.repository.jpa.notification;
 
-record NotificationProjection() {
+import ovh.equino.actracker.domain.Notification;
+import ovh.equino.actracker.domain.exception.ParseException;
+
+record NotificationProjection(String data) {
+
+    Notification<?> toNotification() {
+        try {
+            return Notification.fromJson(data);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
