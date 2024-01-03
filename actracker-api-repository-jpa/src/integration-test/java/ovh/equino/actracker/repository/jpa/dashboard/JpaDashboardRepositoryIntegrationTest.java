abebcb513d1732bc12d626513e42c44cecdfdc5f
Marcin Ciapa <aaryndo@gmail.com>
2024-01-03 21:13:34 +0100

    Extend JPA repository integration tests (#158)
    * Improve JPA repositories integration tests
    
    * Extend add and get repository tests
    
    * Version - patch
    
    * Enhance tag set update test
    
    * Enhance dashboard repository test
    
    * Enhance tag repository tests
    
    * Enhance activity repository test

--- actracker-api-repository-jpa/src/integration-test/java/ovh/equino/actracker/repository/jpa/dashboard/JpaDashboardRepositoryIntegrationTest.java
+++ actracker-api-repository-jpa/src/integration-test/java/ovh/equino/actracker/repository/jpa/dashboard/JpaDashboardRepositoryIntegrationTest.java
@@ -2,21 +2,28 @@ package ovh.equino.actracker.repository.jpa.dashboard;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ovh.equino.actracker.domain.dashboard.Dashboard;
-import ovh.equino.actracker.domain.dashboard.DashboardFactory;
-import ovh.equino.actracker.domain.dashboard.DashboardId;
-import ovh.equino.actracker.domain.dashboard.DashboardTestFactory;
+import ovh.equino.actracker.domain.dashboard.*;
+import ovh.equino.actracker.domain.share.Share;
+import ovh.equino.actracker.domain.tag.TagDto;
+import ovh.equino.actracker.domain.tenant.TenantDto;
 import ovh.equino.actracker.domain.user.User;
 import ovh.equino.actracker.repository.jpa.JpaIntegrationTest;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static java.util.Collections.emptyList;
 import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
+import static ovh.equino.actracker.domain.dashboard.AnalysisMetric.TAG_DURATION;
+import static ovh.equino.actracker.domain.dashboard.GroupBy.SELF;
 import static ovh.equino.actracker.repository.jpa.TestUtil.nextUUID;
 
 abstract class JpaDashboardRepositoryIntegrationTest extends JpaIntegrationTest {
+
+    private static final boolean DELETED = Boolean.TRUE;
 
     private JpaDashboardRepository repository;
     private DashboardFactory dashboardFactory;
@@ -29,13 +36,63 @@ abstract class JpaDashboardRepositoryIntegrationTest extends JpaIntegrationTest 
     }
 
     @Test
-    void shouldAddAndGetDashboard() {
+    void shouldAddAndGetMinimalDashboard() {
         Dashboard expectedDashboard = dashboardFactory.create("dashboard name", emptyList(), emptyList());
         inTransaction(() -> repository.add(expectedDashboard));
         inTransaction(() -> {
             Optional<Dashboard> foundDashboard = repository.get(expectedDashboard.id());
             assertThat(foundDashboard).get().usingRecursiveComparison().isEqualTo(expectedDashboard);
         });
+    }
+
+    @Test
+    void shouldAddAndGetFullDashboard() {
+        Chart chart = new Chart(new ChartId(), "chart name", SELF, TAG_DURATION, Set.of(nextUUID()), !DELETED);
+        Share notResolvedShare = new Share("grantee name");
+        Share resolvedShare = new Share(new User(nextUUID()), "grantee");
+        Dashboard expectedDashboard = dashboardFactory.create(
+                "dashboard name",
+                List.of(chart),
+                List.of(notResolvedShare, resolvedShare)
+        );
+        inTransaction(() -> repository.add(expectedDashboard));
+        inTransaction(() -> {
+            Optional<Dashboard> foundDashboard = repository.get(expectedDashboard.id());
+            assertThat(foundDashboard).get().usingRecursiveComparison().isEqualTo(expectedDashboard);
+        });
+    }
+
+    @Test
+    void shouldAddAndGetMutatedDashboard() {
+        Chart chartToDelete = new Chart(new ChartId(), "chart to delete", SELF, TAG_DURATION, Set.of(nextUUID()), !DELETED);
+        Share notResolvedShareToDelete = new Share("not resolved share to delete");
+        Share resolvedShareToDelete = new Share(new User(nextUUID()), "resolved share to delete");
+
+        Chart newChart = new Chart(new ChartId(), "new chart", SELF, TAG_DURATION, Set.of(nextUUID()), !DELETED);
+        Share newNotResolvedShare = new Share("new not resolved share");
+        Share newResolvedShare = new Share(new User(nextUUID()), "new resolved share");
+
+        Dashboard expectedDashboard = dashboardFactory.create(
+                "old dashboard name",
+                List.of(chartToDelete),
+                List.of(notResolvedShareToDelete, resolvedShareToDelete)
+        );
+
+        expectedDashboard.rename("new dashboard name");
+        expectedDashboard.deleteChart(chartToDelete.id());
+        expectedDashboard.addChart(newChart);
+        expectedDashboard.unshare(notResolvedShareToDelete.granteeName());
+        expectedDashboard.unshare(resolvedShareToDelete.granteeName());
+        expectedDashboard.share(newNotResolvedShare);
+        expectedDashboard.share(newResolvedShare);
+        expectedDashboard.delete();
+
+        inTransaction(() -> repository.add(expectedDashboard));
+        inTransaction(() -> {
+            Optional<Dashboard> foundDashboard = repository.get(expectedDashboard.id());
+            assertThat(foundDashboard).get().usingRecursiveComparison().isEqualTo(expectedDashboard);
+        });
+
     }
 
     @Test
@@ -47,19 +104,55 @@ abstract class JpaDashboardRepositoryIntegrationTest extends JpaIntegrationTest 
     }
 
     @Test
-    void shouldUpdateDashboard() {
-        Dashboard expectedDashboard = dashboardFactory.create("old name", emptyList(), emptyList());
+    void shouldUpdateDashboard() throws SQLException {
+        TenantDto user = newUser().build();
+        TagDto tagToRemove = newTag(user).build();
+        TagDto tagToAdd = newTag(user).build();
+        database().addTags(tagToRemove, tagToAdd);
+
+        Chart chartToDelete = new Chart(new ChartId(), "chart to delete", SELF, TAG_DURATION, Set.of(tagToRemove.id()), !DELETED);
+        Share notResolvedShareToDelete = new Share("not resolved share to delete");
+        Share resolvedShareToDelete = new Share(new User(nextUUID()), "resolved share to delete");
+
+        Chart newChart = new Chart(new ChartId(), "new chart", SELF, TAG_DURATION, Set.of(tagToAdd.id()), !DELETED);
+        Share newNotResolvedShare = new Share("new not resolved share");
+        Share newResolvedShare = new Share(new User(nextUUID()), "new resolved share");
+
+        Dashboard expectedDashboard = dashboardFactory.create(
+                "old dashboard name",
+                List.of(chartToDelete),
+                List.of(notResolvedShareToDelete, resolvedShareToDelete)
+        );
+
         inTransaction(() -> repository.add(expectedDashboard));
-//        inTransaction(() -> {
-        // TODO extend update with additional fields
-        Dashboard dashboard = repository.get(expectedDashboard.id()).get();
-        expectedDashboard.delete();
-        dashboard.delete();
-        repository.save(dashboard);
-//        });
-//        inTransaction(() -> {
-        Optional<Dashboard> foundDashboard = repository.get(expectedDashboard.id());
-        assertThat(foundDashboard).get().usingRecursiveComparison().isEqualTo(expectedDashboard);
-//        });
+
+        inTransaction(() -> {
+            Dashboard dashboard = repository.get(expectedDashboard.id()).get();
+
+            expectedDashboard.rename("new dashboard name");
+            expectedDashboard.deleteChart(chartToDelete.id());
+            expectedDashboard.addChart(newChart);
+            expectedDashboard.unshare(notResolvedShareToDelete.granteeName());
+            expectedDashboard.unshare(resolvedShareToDelete.granteeName());
+            expectedDashboard.share(newNotResolvedShare);
+            expectedDashboard.share(newResolvedShare);
+            expectedDashboard.delete();
+
+            dashboard.rename("new dashboard name");
+            dashboard.deleteChart(chartToDelete.id());
+            dashboard.addChart(newChart);
+            dashboard.unshare(notResolvedShareToDelete.granteeName());
+            dashboard.unshare(resolvedShareToDelete.granteeName());
+            dashboard.share(newNotResolvedShare);
+            dashboard.share(newResolvedShare);
+            dashboard.delete();
+
+            repository.save(dashboard);
+        });
+
+        inTransaction(() -> {
+            Optional<Dashboard> foundDashboard = repository.get(expectedDashboard.id());
+            assertThat(foundDashboard).get().usingRecursiveComparison().isEqualTo(expectedDashboard);
+        });
     }
 }
