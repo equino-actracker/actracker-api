f9c98c7695210404d2616c3b0388904f63ebfc59
Marcin Ciapa <aaryndo@gmail.com>
2024-01-03 17:51:19 +0100

    Enhance activity repository test

--- actracker-api-repository-jpa/src/integration-test/java/ovh/equino/actracker/repository/jpa/activity/JpaActivityRepositoryIntegrationTest.java
+++ actracker-api-repository-jpa/src/integration-test/java/ovh/equino/actracker/repository/jpa/activity/JpaActivityRepositoryIntegrationTest.java
@@ -90,7 +90,7 @@ abstract class JpaActivityRepositoryIntegrationTest extends JpaIntegrationTest {
                 Instant.ofEpochMilli(1),
                 Instant.ofEpochMilli(2),
                 "old activity comment",
-                List.of(new TagId(tagWithMetrics.id())),
+                List.of(new TagId(tagWithMetrics.id()), new TagId(tagToRemove.id())),
                 List.of(new MetricValue(metricToUpdate.id(), TEN), new MetricValue(metricToUnset.id(), ONE))
         );
 
@@ -123,26 +123,47 @@ abstract class JpaActivityRepositoryIntegrationTest extends JpaIntegrationTest {
     @Test
     void shouldUpdateActivity() throws SQLException {
         TenantDto user = newUser().build();
-        MetricDto metric = newMetric(user).build();
-        TagDto tag = newTag(user).withMetrics(metric).build();
-        database().addTags(tag);
+        TagDto tagToRemove = newTag(user).build();
+        TagDto tagToAdd = newTag(user).build();
+        MetricDto metricToSet = newMetric(user).build();
+        MetricDto metricToUpdate = newMetric(user).build();
+        MetricDto metricToUnset = newMetric(user).build();
+        TagDto tagWithMetrics = newTag(user).withMetrics(metricToSet, metricToUpdate, metricToUnset).build();
+        database().addTags(tagWithMetrics, tagToRemove, tagToAdd);
 
         Activity expectedActivity = activityFactory.create(
-                "old title",
-                null,
-                null,
-                null,
-                emptyList(),
-                emptyList()
+                "old activity title",
+                Instant.ofEpochMilli(1),
+                Instant.ofEpochMilli(2),
+                "old activity comment",
+                List.of(new TagId(tagWithMetrics.id()), new TagId(tagToRemove.id())),
+                List.of(new MetricValue(metricToUpdate.id(), TEN), new MetricValue(metricToUnset.id(), ONE))
         );
-
         inTransaction(() -> repository.add(expectedActivity));
-
 
         inTransaction(() -> {
             Activity activity = repository.get(expectedActivity.id()).get();
 
+            expectedActivity.rename("new activity title");
+            expectedActivity.finish(Instant.ofEpochMilli(4));
+            expectedActivity.start(Instant.ofEpochMilli(3));
+            expectedActivity.updateComment("new activity comment");
+            expectedActivity.assignTag(new TagId(tagToAdd.id()));
+            expectedActivity.removeTag(new TagId(tagToRemove.id()));
+            expectedActivity.setMetricValue(new MetricValue(metricToSet.id(), ZERO));
+            expectedActivity.setMetricValue(new MetricValue(metricToUpdate.id(), ONE));
+            expectedActivity.unsetMetricValue(new MetricId(metricToUnset.id()));
             expectedActivity.delete();
+
+            activity.rename("new activity title");
+            activity.finish(Instant.ofEpochMilli(4));
+            activity.start(Instant.ofEpochMilli(3));
+            activity.updateComment("new activity comment");
+            activity.assignTag(new TagId(tagToAdd.id()));
+            activity.removeTag(new TagId(tagToRemove.id()));
+            activity.setMetricValue(new MetricValue(metricToSet.id(), ZERO));
+            activity.setMetricValue(new MetricValue(metricToUpdate.id(), ONE));
+            activity.unsetMetricValue(new MetricId(metricToUnset.id()));
             activity.delete();
 
             repository.save(activity);
