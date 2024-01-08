6a669816ee008722f7a77c89104617ddcd3ba603
Marcin Ciapa <aaryndo@gmail.com>
2024-01-08 20:52:29 +0100

    Remove unnecessary code from JPA module

--- actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/notification/JpaNotificationRepository.java
+++ actracker-api-repository-jpa/src/main/java/ovh/equino/actracker/repository/jpa/notification/JpaNotificationRepository.java
@@ -1,19 +1,14 @@
 package ovh.equino.actracker.repository.jpa.notification;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import ovh.equino.actracker.domain.Notification;
 import ovh.equino.actracker.notification.outbox.NotificationRepository;
 import ovh.equino.actracker.repository.jpa.JpaDAO;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static java.util.Objects.isNull;
-import static java.util.stream.Collectors.toList;
 
 class JpaNotificationRepository extends JpaDAO implements NotificationRepository {
 
@@ -27,16 +22,16 @@ class JpaNotificationRepository extends JpaDAO implements NotificationRepository
     public void save(Notification<?> notification) {
         NotificationEntity notificationEntity = notificationMapper.toEntity(notification);
         entityManager.merge(notificationEntity);
-        entityManager.flush();  // TODO ?there is something wrong with transactionality if this needs to be called?
+        entityManager.flush();  // TODO ?there is something wrong with transactional if this needs to be called?
     }
 
     @Override
     public Optional<Notification<?>> get(UUID notificationId) {
         NotificationEntity notificationEntity = entityManager.find(NotificationEntity.class, notificationId.toString());
-        if(isNull(notificationEntity)) {
+        if (isNull(notificationEntity)) {
             return Optional.empty();
         }
-        entityManager.refresh(notificationEntity); // TODO ?there is something wrong with transactionality if this needs to be called?
+        entityManager.refresh(notificationEntity); // TODO ?there is something wrong with transactional if this needs to be called?
         return Optional.of(notificationEntity)
                 .map(notificationMapper::toDto);
     }
