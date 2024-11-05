1e74028600a6d35cbc5a55ce6b3e80075bf06abd
Marcin Ciapa <aaryndo@gmail.com>
2024-11-05 21:21:49 +0100

    Introduce application service instrumentation (#169)
    * Introduce application service instrumentation
    
    * Expose percentiles for time metrics
    
    * Add prometheus annotations
    
    * Revert "Add prometheus annotations"
    
    This reverts commit c848c493d12d0f3aea3514150c632bc91ff14316.
    
    * Distinguish metrics between pods
    
    * update host name to localhost

---
+++ actracker-api-main-springboot/src/main/java/ovh/equino/actracker/main/springboot/configuration/instrumentation/MetricsCollector.java
@@ -0,0 +1,43 @@
+package ovh.equino.actracker.main.springboot.configuration.instrumentation;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Callable;
+
+@Component
+public class MetricsCollector {
+
+    @Value("${actracker-api.environment:local}")
+    private String applicationEnvironment;
+
+    @Value("${HOSTNAME:localhost}")
+    private String hostname;
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    public Object measureAndExecute(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String metricName = "actracker-api.%s.%s.%s.%s"
+                .formatted(applicationEnvironment, hostname, className, methodName);
+
+        Callable<Object> procedure = () -> {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        try {
+            return meterRegistry.timer(metricName).recordCallable(procedure);
+        } catch (RuntimeException e) {
+            throw e.getCause();
+        }
+    }
+}
